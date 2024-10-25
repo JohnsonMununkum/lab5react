c797960c2374c7c469e505c9ed924f4a451ea4be
Johnson <G00419319@atu.ie>
2024-10-25 11:02:15 +0000

    exercise 5

--- server.js
+++ server.js
@@ -18,6 +18,38 @@ app.get('/hello/:name/:surname', (req, res) => {
     res.send(`Hello ${name} ${surname}`);
 });
 
+app.get('/api/movies', (req, res) => {
+    const movies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://example.com/poster1.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://example.com/poster2.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://example.com/poster3.jpg"
+        }
+    ];
+    //sending json back
+   // res.json({ movies });
+   res.status(200).json({ myMovies:movies });
+
+
+
+});
+
 //error handling to catch any server errors when the server is run
 //will print out something is wrong if errors occur
 app.use((err, req, res, next) => {
@@ -25,6 +57,8 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something went wrong!');
 });
 
+//adding status code to the response
+//res.status(201).json({ movies });
 //server will sit there and listen for a htp request to come in
 //server is sitting up and listening on the port we have defined here port 4000
 //app.listen sets up serever to listen for a request
